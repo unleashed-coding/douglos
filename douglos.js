506f3feb474348762db2ed7c85bccb6174451d45
Ed <eudes.scar@gmail.com>
2019-02-13 15:29:02 -0300

    ES6 douglos

--- douglos.js
+++ douglos.js
@@ -1,7 +1,8 @@
-var imgs = document.getElementsByTagName('img');
+window.onload = () => {
+  const imgs = [...document.getElementsByTagName('img')]
 
-for(var i = 0; i < imgs.length; i++) {
-  if (Math.random() >= 0.9) {
-    imgs[i].src="https://i.imgur.com/XSrHlah.jpg"
-  }
+  imgs.forEach(img => {
+    if (Math.random() >= 0.9)
+      img.src = "https://i.imgur.com/XSrHlah.jpg"
+  })
 }
